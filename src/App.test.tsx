e50dfd25b14adc9c9eab936d4da799138cb1a38d
Jonas Antunes <jonas.antunes@globant.com>
2023-09-29 09:55:39 -0300

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,27 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen, waitFor } from "@testing-library/react";
+import { Dashboard } from "./components/Dashboard";
+import { DataTable } from "./components/DataTable";
+import usersMock from "./mocked.json";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Create Item/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", async () => {
+  render(<Dashboard />);
+  const ButtonElement = screen.getByText(/Create Item/i);
+  await waitFor(() => {
+    expect(ButtonElement).toBeInTheDocument();
+  });
+});
+
+describe("data table", () => {
+  it("should render table with 20 users", () => {
+    render(
+      <DataTable
+        users={usersMock}
+        headers={["ID", "First Name", "Last Name", "E-mail"]}
+      />
+    );
+
+    const rows = screen.getAllByRole("row");
+
+    expect(rows.length).toBe(usersMock.length + 1);
+  });
 });
